0630c9b34002e1573a4861826d3c7aac7f7617bc
Stella40k <bernardstella0704@gmail.com>
2025-09-05 12:59:59 -0300

    feat: auth middleware terminado

---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+export const authMiddleware =(req, res, next)=>{
+    try {
+        const token = req.cookies.token;
+
+        //verificacion de la existencia de ese token
+        if(!token){
+            return res.status(401).json({message: "usuario no autenticado, inicie sesion", error});
+        }
+
+        //verifica y codifica el token con la firma
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        //junta la info del user a la solicitud, pedir mas explicacion igual
+        req.user = decode
+        next();
+    } catch (error) {
+        return res.status(401).json({message:"token invalido o caducado, inicie sesion nuevamente"});
+    }
+}
